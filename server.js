b6960d1eeec58cbbea756fea9267520607dfa536
Patrick St. <steiner_patrick@outlook.com>
2024-02-25 17:46:19 +0100

    added api functions for getting all and individual technologies as well as adding a new technology to the db.

--- server.js
+++ server.js
@@ -7,21 +7,45 @@ require('dotenv').config();
 
 server.use(bodyParser.json());
 
-const connectionString = ''; // connection string to mongodb with password not visible!
+const connectionString =
+    `mongodb+srv://patstein:${process.env.MONGO_DB_PW}@patradar.cnfbnq1.mongodb.net/?retryWrites=true&w=majority&appName=patRadar`;
 
 server.use((req, res, next) => {
-    console.log('TS:', new Date().toISOString());
+    console.log(`[${new Date().toISOString()}]`);
     next();
 });
 
-server.use((req, res, next) => {
-    console.log(`process.env.USER_ID: ${process.env.USER_ID}`);
-    next();
+// GET /technologies - returns all technologies in array
+server.get('/technologies', async (req, res) => {
+    const client = await mongoClient.connect(connectionString);
+    const db = client.db('patRadar');
+    const collection = db.collection('technologies');
+    const result = await collection.find({}).toArray();
+    res.send(result);
 });
 
-server.use('/request-type', (req, res, next) => {
-    console.log('Request type: ', req.method);
-    next();
+// GET /technologies/{id} - returns technology with this id if it exists, otherwise fail message
+server.get('/technologies/:id', async (req, res) => {
+    const client = await mongoClient.connect(connectionString);
+    const db = client.db('patRadar');
+    const collection = db.collection('technologies');
+    const result = await collection.findOne({ _id: objectId(req.params.id) });
+    if (result) {
+        res.send(result);
+    } else {
+        res.status(404);
+    }
+    res.end();
+});
+
+// POST /technologies - saves new technology to array
+server.post('/technologies', async (req, res) => {
+    const client = await mongoClient.connect(connectionString);
+    const db = client.db('patRadar');
+    const collection = db.collection('technologies');
+    await collection.insertOne(req.body);
+    res.status(201);
+    res.end();
 });
 
 server.get('/request-type', (req, res) => {
@@ -35,6 +59,4 @@ server.get('/', (req, res) => {
 server.listen(9999, () => {
     console.log('patRadar server is listening on port 9999...');
 });
-
-// useless comment
 
