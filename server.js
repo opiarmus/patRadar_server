52946ea12c48564ab8f4dc3111e44e48b737eeb7
Patrick St. <steiner_patrick@outlook.com>
2024-02-25 16:26:37 +0100

    setup web server with express, mongodb, body-parser. only the main construct stands. no real functionality yet

---
+++ server.js
@@ -0,0 +1,32 @@
+const express = require('express');
+const mongoClient = require('mongodb').MongoClient;
+const objectId = require('mongodb').ObjectId
+const bodyParser = require('body-parser');
+const server = express();
+
+server.use(bodyParser.json());
+
+const connectionString = ''; // connection string to mongodb with password not visible!
+
+server.use((req, res, next) => {
+    console.log('TS:', new Date().toISOString());
+    next();
+});
+
+server.use('/request-type', (req, res, next) => {
+    console.log('Request type: ', req.method);
+    next();
+});
+
+server.get('/request-type', (req, res) => {
+    res.send(req.method.toString());
+})
+
+server.get('/', (req, res) => {
+    res.send('Successful response.');
+});
+
+server.listen(9999, () => {
+    console.log('patRadar server is listening on port 9999...');
+});
+
